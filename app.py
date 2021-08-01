601ade1560c1292e636ea99da9a2f0b8bdbdb85f
Simran Bhoneja <spbhoneja@gmail.com>
2021-08-01 13:01:29 +0530

    my commit

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db_user = "root"
+db_pass = "3009"
+db_name = "my_notes"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://{}:{}@localhost/{}".format(db_user, db_pass, db_name)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    notes_sql = "Select * from notes"
+    notes = db.session.execute(notes_sql)
+    print(type(notes))
+    for note in notes:
+        print(note)
+    return render_template('index.html', notes = notes)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
