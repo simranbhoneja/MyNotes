f7fce5dca6e8eaeba760fe7eb873a8bba4d27941
Simran Bhoneja <spbhoneja@gmail.com>
2021-08-08 13:16:37 +0530

    templates refactored

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 
 db_user = "root"
@@ -12,15 +13,93 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def index():
-    notes_sql = "Select * from notes"
+    notes_sql = "Select * from notes where deleted_at is null"
     notes = db.session.execute(notes_sql)
-    print(type(notes))
-    for note in notes:
-        print(note)
+    # print(type(notes))
+    # for note in notes:
+    #     print(note)
     return render_template('index.html', notes = notes)
 
+@app.route("/create", methods=['GET', 'POST'])
+def create():
+
+    if request.method == 'GET':
+        folders_sql = "Select * from folder"
+        folders = db.session.execute(folders_sql)
+        return render_template('create.html', folders = folders)
+    elif request.method == 'POST':
+
+        form = request.form
+        params = {
+            "title" : form['title'],
+            "content" : form.get('title', ''),
+            "folder_id" : form.get('folder_id', ''),
+        }
+        if not params['folder_id']:
+            params['folder_id'] = None
+        
+        sql = f"insert into notes (`title`, `content`, `folder_id`) values(:title, :content, :folder_id)"
+        
+        db.session.execute(sql, params)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'GET':
+        folders_sql = "Select * from folder"
+        folders = db.session.execute(folders_sql)
+        note_sql = "Select * from notes where id= :id and deleted_at is null"
+        note = db.session.execute(note_sql, {"id":id}).fetchone()
+        
+        if not note:
+            return redirect(url_for('error', code=404))
+        return render_template('update.html', folders = folders, note = note)
+    
+    elif request.method == 'POST':
+        form = request.form
+        params = {
+            "title" : form['title'],
+            "content" : form.get('content', ''),
+            "folder_id" : form.get('folder_id', ''),
+            "id": id,
+        }
+        if not params['folder_id']:
+            params['folder_id'] = None
+        
+        sql = f"update notes set title=:title, content=:content, folder_id=:folder_id where id=:id"
+        
+        res = db.session.execute(sql, params)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+@app.route("/delete", methods=['POST'])
+def delete(id):
+    if request.method == 'POST':
+        try:
+            id = request.form.get('id', None)
+            if not id:
+                return redirect('error', code=404)
+            sql = f"update notes set deleted_at = now() where id=:id"
+            db.session.execute(sql, {"id": id})
+            db.session.commit()
+        except (Exception):
+            return redirect(url_for('error', code=404))
+        return redirect(url_for('index'))
+
+@app.route("/error/<code>")
+def error(code):
+    codes = {
+        "404": "404 Not Found",
+    }
+    return render_template("error.html", message = codes.get(code, "Invalid Request"))
+
+@app.route("/thrash")
+def thrash():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ db/db_mynotes.sql
@@ -0,0 +1,44 @@
+create database my_notes;
+
+use my_notes;
+
+create table notes (
+	`id` int primary key auto_increment,
+	`title` varchar(500) not null,
+    `content` text not null,
+    `is_complete` tinyint default 0,
+    `folder_id` int default null,
+    `created_at` datetime not null default current_timestamp(),
+    `updated_at` datetime not null default current_timestamp() on update now(),
+    `deleted_at` datetime default null 
+);
+
+create table folder (
+	`id` int primary key auto_increment,
+    `name` varchar(500) not null,
+    `created_at` datetime not null default current_timestamp(),
+    `updated_at` datetime not null default current_timestamp() on update now(),
+    `deleted_at` datetime default null 
+);
+
+alter table notes
+ADD CONSTRAINT FK_FolderId
+foreign key (`folder_id`) references folder(`id`);
+
+insert into notes  (`title`, `content`) values 
+('Note 1', 'This is my first node'),
+('Note 2', 'This is my second node'),
+('Note 3', 'This is my third node'),
+('Note 4', 'This is my Fourth node');
+
+select * from notes;
+select * from folder;
+
+insert into folder (`id`, `name`) values
+('1', 'Django'),
+('2', 'JAVA'),
+('3', 'Python'),
+('4', 'Web Development');
+
+update notes set deleted_at = null where id > 0;
+	
